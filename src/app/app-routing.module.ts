59618be5176d0afed042ea0abddebe54ce69edcf
massndongo86@gmail.com <massndongo86@gmail.com>
2022-08-27 16:34:04 +0000

    [Ajout des fonts] - [Mise en place Navbar - Header- timer]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accueil', component: HomeComponent },
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
