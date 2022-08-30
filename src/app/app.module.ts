c14054d2c9d767be513ca8405942082e3996dad0
Abdul Samad <oumarlam154@gmail.com>
2022-08-30 03:27:13 +0000

    Ajout de la page speakers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { SpeakersComponent } from './components/speakers/speakers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    SpeakersComponent
   ],
   imports: [
     BrowserModule,
