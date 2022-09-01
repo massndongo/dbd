b347aba4635ac1ba1a5f8ef0d484bf9bc4fc0772
Abdul Samad <oumarlam154@gmail.com>
2022-09-01 16:50:57 +0000

    Ajout de la page des présentateurs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SpeakersComponent } from './components/speakers/speakers.component';
 
 const routes: Routes = [
   { path: 'accueil', component: HomeComponent },
   { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+  { path: 'speakers', component: SpeakersComponent },
 ];
 
 @NgModule({
